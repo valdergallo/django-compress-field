fd4dd431176ade6e2d7b109365d77ac4617e5a1d
Valder Gallo <valdergallo@gmail.com>
2014-05-31 14:15:14 -0300

    delete old files

--- compress_storage/zipcompress.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from .base import FileCompressorField
-
-class ZipField(FileCompressorField):
-    """
-    ZipField - auto zip content after file was saved on server
-    """
-    compress_ext = 'zip'
