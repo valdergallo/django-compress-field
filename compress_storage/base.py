db8dde17e65d4e7308ccd677f3a77769aa73c5be
Valder Gallo <valdergallo@gmail.com>
2014-05-31 14:14:31 -0300

    update base to use FieldFile

--- compress_storage/base.py
+++ compress_storage/base.py
@@ -4,76 +4,85 @@ try:
 except ImportError:
     task = None
 
-import zipfile
 import os
-
 from django.conf import settings
-from django.db.models import FileField
-from django.core.files.storage import Storage
+from django.db.models.fields.files import FieldFile
+
 FILE_COMPRESS_DELETE_OLD_FILE = getattr(settings, 'FILE_COMPRESS_DELETE_OLD_FILE', True)
 
 
-class CompressStorage(Storage):
-    compress_ext = 'zip'
+class CompressFieldFile(FieldFile):
+    compress_ext  = None
 
-    def _save(self, name, content):
-        return super(CompressStorage, self)._save(name, content)
-
-
-class FileCompressorField(FileField):
-    compress_ext = 'zip'
-
-    def is_compressed(self, file_name):
-        basename, ext = os.path.splitext(file_name)
+    def _is_compressed(self):
+        basename, ext = os.path.splitext(self.name)
         return self.compress_ext == ext
+    is_compressed = property(_is_compressed)
 
-    @property
-    def filefield_instance(self):
-        return getattr(self.model_instance, self.get_attname())
+    def _compress_name(self):
+        basename, ext = os.path.splitext(self.name)
+        return basename + ('.' + self.compress_ext)
+    compress_name = property(_compress_name)
+
+    # In addition to the standard File API, FieldFiles have extra methods
+    # to further manipulate the underlying file, as well as update the
+    # associated model instance.
+    def save(self, name, content, save=True):
+        name = self.field.generate_filename(self.instance, name)
+        self.name = self.storage.save(name, content)
+        setattr(self.instance, self.field.name, self.name)
+
+        # Update the filesize cache
+        self._size = content.size
+        self._committed = True
+
+        # Save the object because it has changed, unless save is False
+        if save:
+            self.instance.save()
+    save.alters_data = True
+
+    def delete(self, save=True):
+        # Only close the file if it's already open, which we know by the
+        # presence of self._file
+        if hasattr(self, '_file'):
+            self.close()
+            del self.file
+
+        self.storage.delete(self.name)
+
+        self.name = None
+        setattr(self.instance, self.field.name, self.name)
+
+        # Delete the filesize cache
+        if hasattr(self, '_size'):
+            del self._size
+        self._committed = False
+
+        if save:
+            self.instance.save()
+    delete.alters_data = True
 
     def get_compress_name(self):
-        basename, ext = os.path.splitext(self.filefield_instance.name)
+        basename, ext = os.path.splitext(self.name)
         return basename + ('.' + self.compress_ext)
 
     def compress_content(self, delete_old_file=True):
         """
         Method to change for new implementations
         """
-        compress_file_fullname = self.get_compress_name()
+        raise NotImplementedError()
 
-        if self.is_compressed(compress_file_fullname):
-            return
+    def _update_filefield_name(self):
+        # update field value
+        setattr(self.instance, self.field.name, self.compress_name)
+        self.instance.save()
 
-        in_memory_zip = file(compress_file_fullname, 'w')
-        ziped = zipfile.ZipFile(in_memory_zip, "w", zipfile.ZIP_DEFLATED)
-        ziped.write(self.filefield_instance.name)
-        ziped.close()
-
-        self.save_compress_content(ziped, delete_old_file)
-        return ziped
-
-    # def save_compress_content(self, file_content, delete_old_file=True):
-    #     file = super(FileField, self).pre_save(self.model_instance, True)
-    #     setattr(file, '_committed', True)
-    #     #if delete_old_file:  # remove old file
-    #         # os.unlink(self.filefield_instance.name)
-    #         # self.storage.delete(self.name)
-
-    #     compress_name = self.get_compress_name()
-    #     # save zipfile on database
-    #     if file and not file._committed:
-    #         return file.save(compress_name, file_content, save=False)
-    #     return file
-
-    def compress_file(self, model_instance=None, async=True, delete_old_file=True):
+    def compress(self, async=True, delete_old_file=True):
         if async and task:
             file_content = task(self.compress_content).delay(delete_old_file=delete_old_file)
         else:
             file_content = self.compress_content(delete_old_file=delete_old_file)
 
-        return file_content
+        self._update_filefield_name()
 
-    # def pre_save(self, model_instance, add):
-    #     # add on post_save auto compress content field
-    #     self.model_instance = model_instance
-    #     self.compress_file(async=True, delete_old_file=FILE_COMPRESS_DELETE_OLD_FILE)
+        return file_content
