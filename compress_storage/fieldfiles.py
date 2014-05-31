30a380da4e97aed698ca0ffc669e83705e87a404
Valder Gallo <valdergallo@gmail.com>
2014-05-31 14:20:47 -0300

    remove import django File

--- compress_storage/fieldfiles.py
+++ compress_storage/fieldfiles.py
@@ -3,7 +3,6 @@ import zipfile
 import os
 from .base import CompressFieldFile
 from django.conf import settings
-from django.core.files.base import File
 
 
 class ZipCompressFieldFile(CompressFieldFile):
