985d7df7fbc031d4eb57e322f269968c7d6745ea
Valder Gallo <valdergallo@gmail.com>
2014-05-31 15:09:12 -0300

    no need update default save and delete

--- compress_storage/base.py
+++ compress_storage/base.py
@@ -25,44 +25,6 @@ class CompressFieldFile(FieldFile):
         return basename + ('.' + self.compress_ext)
     compress_name = property(_compress_name)
 
-    # In addition to the standard File API, FieldFiles have extra methods
-    # to further manipulate the underlying file, as well as update the
-    # associated model instance.
-    def save(self, name, content, save=True):
-        name = self.field.generate_filename(self.instance, name)
-        self.name = self.storage.save(name, content)
-        setattr(self.instance, self.field.name, self.name)
-
-        # Update the filesize cache
-        self._size = content.size
-        self._committed = True
-
-        # Save the object because it has changed, unless save is False
-        if save:
-            self.instance.save()
-    save.alters_data = True
-
-    def delete(self, save=True):
-        # Only close the file if it's already open, which we know by the
-        # presence of self._file
-        if hasattr(self, '_file'):
-            self.close()
-            del self.file
-
-        self.storage.delete(self.name)
-
-        self.name = None
-        setattr(self.instance, self.field.name, self.name)
-
-        # Delete the filesize cache
-        if hasattr(self, '_size'):
-            del self._size
-        self._committed = False
-
-        if save:
-            self.instance.save()
-    delete.alters_data = True
-
     def get_compress_name(self):
         basename, ext = os.path.splitext(self.name)
         return basename + ('.' + self.compress_ext)
