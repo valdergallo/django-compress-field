9907aac5ea5426f6240288a832876088edb3d8ba
Valder <valdergallo@gmail.com>
2014-06-03 15:46:30 -0300

    update tests

--- example/core/models.py
+++ example/core/models.py
@@ -5,7 +5,7 @@ from compress_storage import ZipFileField
 class MyContent(models.Model):
     name = models.CharField(max_length=150)
     create_date = models.DateTimeField(auto_now=True)
-    upload_file = ZipFileField(upload_to='mycontent/')
+    upload_file = ZipFileField(upload_to='mycontent/2014/06/03/')
 
     def __unicode__(self):
         return self.name
--- example/core/tests.py
+++ example/core/tests.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import shutil
 from django.test import TestCase
 from example.core.models import MyContent
 from django.core.files.base import File
@@ -12,18 +13,21 @@ except NameError:
 BASEDIR = os.path.dirname(__file__)
 FIXTURE = os.path.join(BASEDIR, 'data', 'text.txt')
 
+UPLOADED_FILE_FULL_PATH = os.path.join(settings.MEDIA_ROOT, 'mycontent/2014/06/03/', 'test_fixture.txt')
+UPLOADED_ZIPFILE_FULL_PATH = os.path.join(settings.MEDIA_ROOT, 'mycontent/2014/06/03/', 'test_fixture.zip')
+
+UPLOADED_FILE_PATH = os.path.join('mycontent/2014/06/03/', 'test_fixture.txt')
+UPLOADED_ZIPFILE_PATH = os.path.join('mycontent/2014/06/03/', 'test_fixture.zip')
+UPLOADED_ZIPFILE2_PATH = os.path.join('mycontent/2014/06/03/', 'test_fixture_1.zip')
+
 
 class TestCompressTestCase(TestCase):
 
     def setUp(self):
         self.dummyfile = File(open(FIXTURE, 'r'), name='test_fixture.txt')
-        self.uploaded_file_path = os.path.join(settings.MEDIA_ROOT,
-                                               'mycontent',
-                                               'test_fixture.txt')
-        try:
-            os.unlink(self.uploaded_file_path)
-        except (OSError, AttributeError, ioError):
-            pass
+
+    def tearDown(self):
+        shutil.rmtree(os.path.join(settings.MEDIA_ROOT, 'mycontent'), ignore_errors=True)
 
     def create_my_content(self):
         my_content = MyContent()
@@ -34,21 +38,20 @@ class TestCompressTestCase(TestCase):
 
     def test_save_file_on_model(self):
         my_content = self.create_my_content()
-
-        self.assertEqual(my_content.upload_file.name, 'mycontent/test_fixture.txt')
+        self.assertEqual(my_content.upload_file.name, UPLOADED_FILE_PATH)
 
     def test_save_zipfile_on_model(self):
         my_content = self.create_my_content()
         my_content.upload_file.compress()
 
-        self.assertEqual(my_content.upload_file.name, 'mycontent/test_fixture.zip')
+        self.assertEqual(my_content.upload_file.name, UPLOADED_ZIPFILE_PATH)
 
     def test_is_compress_has_updated_register(self):
         my_content = self.create_my_content()
         my_content.upload_file.compress()
 
         my_content = MyContent.objects.get(id=my_content.id)
-        self.assertEqual(my_content.upload_file.name, 'mycontent/test_fixture.zip')
+        self.assertEqual(my_content.upload_file.name, UPLOADED_ZIPFILE_PATH)
 
     def test_if_is_compressed_must_return_true(self):
         my_content = self.create_my_content()
@@ -61,10 +64,19 @@ class TestCompressTestCase(TestCase):
 
     def test_compress_name(self):
         my_content = self.create_my_content()
-        self.assertEqual(my_content.upload_file.compress_name, 'mycontent/test_fixture.zip')
+        self.assertEqual(my_content.upload_file.compress_name, UPLOADED_ZIPFILE_PATH)
 
     def test_if_compress_delete_file_uncompress(self):
         my_content = self.create_my_content()
         my_content.upload_file.compress()
 
-        self.assertFalse(os.path.exists(self.uploaded_file_path))
+        self.assertFalse(os.path.exists(UPLOADED_FILE_PATH))
+
+    def test_if_zip_is_not_override_names(self):
+        my_content = self.create_my_content()
+        my_content.upload_file.compress()
+        self.assertEqual(my_content.upload_file.name, UPLOADED_ZIPFILE_PATH)
+
+        my_content2 = self.create_my_content()
+        my_content2.upload_file.compress()
+        self.assertEqual(my_content2.upload_file.name.replace('\\', '/'), UPLOADED_ZIPFILE2_PATH)
